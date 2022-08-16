411df0de35f4a884138b968df99cd09846ae47af
pi <zhamanbaev.d@mail.ru>
2022-08-16 21:39:30 +0600

    add files

--- app/flashdetector.py
+++ app/flashdetector.py
@@ -1,24 +1,23 @@
 def FlashDetector(pref, mountedFlash): #pref - путь к флешкам,
-    import os, time, logging
-    from app.Log.logger import Logger
-    Logger()
+    import os, time, logging,sys
     mountDir = os.listdir(pref)
     newFlashes = []
+    while mountDir == mountedFlash:
+        mountDir = os.listdir(pref)
+        time.sleep(0.5)
     for f in mountedFlash:
         if f not in mountDir:
             mountedFlash.remove(f)
             logging.info(f"flash {f} isn't active")
-    while mountDir == mountedFlash:
-        mountDir = os.listdir(pref)
-        time.sleep(0.5)
     for dir in mountDir:
         findmnt = os.popen(f"findmnt {pref}{dir}")
         res = findmnt.readlines()
         if len(res) == 0:
-            mountedFlash.append(dir)
+            if dir not in mountedFlash:
+                mountedFlash.append(dir)
     for flash in mountDir:
         if flash not in mountedFlash:
             newFlashes.append(flash)
             mountedFlash.append(flash)
             logging.info(f"Found a new flash! Endpoint:{pref}{flash}")
-    return mountedFlash, newFlashes
+    return (mountedFlash, newFlashes)
