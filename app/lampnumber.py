2444d2feaf0b52ff407f4f393f483756c5f14cf2
dauletZ <110877286+dauletZ@users.noreply.github.com>
2022-10-17 18:10:23 +0600

    Update lampnumber.py

--- app/lampnumber.py
+++ app/lampnumber.py
@@ -7,8 +7,9 @@ def GetLampNumber(newFlash):
     try:
         files = os.listdir(newFlash)
     except:
-        time.sleep(1.5)
-        files = os.listdir(newFlash)
+        if os.path.exists(newFlash) == False:
+            logging.info(f"No such file or directory {newFlash}")
+            return 0
     for file in files:
         if file.startswith("CMC15_") and file.endswith("txt"):
             if len(newFlash) > 6:
